cd13a0e9c529d2e0dc9f59e93baa4c1ef1eb2a19
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-05 22:23:27 -0800

    Random mix of another container I found, just randomly testing stuff right now PT2

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,10 @@
 set -eux
 echo "---------- Running Setup"
 
-sudo apt update
-sudo apt install -y vim tmux kitty i3
+sudo pacman -Syu
+sudo pacman --noconfirm vim tmux kitty
 
-curl -LJO https://raw.githubusercontent.com/luciendberkeley/arch-codespace-template/refs/heads/main/.config/.vimrc
+curl -LJO https://raw.githubusercontent.com/luciendberkeley/codespace-script/refs/heads/main/.config/.vimrc
 mv ./.vimrc ~/
 
 echo "Setup done"
