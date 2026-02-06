3c4997547d426033ec97e87289bd4d7792769a8c
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-05 22:10:53 -0800

    added tmux kitty and i3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,7 @@ set -eux
 echo "---------- Running Setup"
 
 sudo apt update
-sudo apt install -y vim
+sudo apt install -y vim tmux kitty i3
 
 curl -LJO https://raw.githubusercontent.com/luciendberkeley/arch-codespace-template/refs/heads/main/.config/.vimrc
 mv ./.vimrc ~/
