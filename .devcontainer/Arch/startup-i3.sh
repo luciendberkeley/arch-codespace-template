280ad0a885b5d75e9ab65e51b4d3decb0139e4f1
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-07 06:05:17 +0000

    Just trying things

--- .devcontainer/Arch/startup-i3.sh
+++ .devcontainer/Arch/startup-i3.sh
@@ -13,7 +13,7 @@ echo "Starting VNC server..."
 x11vnc -display :0 -nopw -forever -shared -rfbport 5900 &
 
 echo "Starting noVNC on port 6080..."
-/usr/share/noVNC/utils/launch.sh --vnc localhost:5900 --listen 0.0.0.0 --web /usr/share/noVNC &
+/usr/share/noVNC/utils/novnc_proxy.sh --vnc localhost:5900 --listen 0.0.0.0:6800 --web /usr/share/noVNC &
 
 echo ""
 echo "GUI environment is ready!"
